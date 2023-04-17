bf5bbc656486f609599e8bec26e3b690c1455d8e
sofiansmendes <ana.sofia.ns.mendes@gmail.com>
2023-04-17 18:29:05 +0200

    ex22

--- src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
@@ -1,3 +1,14 @@
 <script>
-    
+    let count = 0;
+
+function handleClick(event) {
+    count += 1;
+}
+
+function resetClick(event) {
+    count = 0;
+}
 </script>
+
+<button class="btn btn-info" on:click={handleClick}> Count: {count} </button>
+<button class="btn btn-warning" on:click={resetClick}> Reset </button>
